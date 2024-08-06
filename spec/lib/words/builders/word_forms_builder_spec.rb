34ecdd9e3212bd46b230e5dea24764fff0c99c50
Alina Latypova <alina.latypova@kyero.com>
2024-08-07 01:17:50 +0200

    fix rubocop

--- spec/lib/words/builders/word_forms_builder_spec.rb
+++ spec/lib/words/builders/word_forms_builder_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Words::Builders::WordFormsBuilder do
   let(:base_item) { { "form" => "subst:sg:gen" } }
@@ -8,22 +8,22 @@ describe Words::Builders::WordFormsBuilder do
       { "form" => "subst:pl:nom", "word" => "koty" }
     ]
   end
-  
+
   subject { described_class.call(base_item, items) }
 
-  describe '#call' do
+  describe "#call" do
     context 'when form_name is "subst"' do
       before { allow_any_instance_of(Words::Builders::Nouns::Builder).to receive(:call).and_return("expected_result") }
 
-      it 'calls the correct builder class with filtered items' do
+      it "calls the correct builder class with filtered items" do
         expect(subject).to eq("expected_result")
         expect(Nouns::Builder).to have_received(:call).with([
-          { "form" => "subst:sg:gen", "word" => "kota" }
-        ])
+                                                              { "form" => "subst:sg:gen", "word" => "kota" }
+                                                            ])
       end
     end
 
-    context 'when form_name is not in BUILDER_CLASSES' do
+    context "when form_name is not in BUILDER_CLASSES" do
       let(:base_item) { { "form" => "unknown:sg:gen" } }
 
       it { is_expected.to raise_error(KeyError) }
