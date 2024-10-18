14cb38695c8f200eddf297763b17e809f4c399f0
Alina Latypova <alina.latypova@kyero.com>
2024-10-18 19:21:54 +0200

    more refactor

--- app/interactors/word_info/verb/finite/format/joined_content.rb
+++ app/interactors/word_info/verb/finite/format/joined_content.rb
@@ -6,7 +6,7 @@ module WordInfo
           delegate :processed_items, to: :context
 
           def call
-            context.formatted_text = processed_items.map { |sub_item| Text.call(item: sub_item).text }.join("\n")
+            context.formatted_text = processed_items.map { |sub_item| Body.call(item: sub_item).body }.join("\n")
           end
         end
       end
